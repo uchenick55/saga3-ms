584dda75cd71789787e3c600e019d6d8a9ddf251
uchenick55 <uchenick55@yandex.ru>
2023-06-13 14:24:20 +0300

    Updates

---
+++ static/js/189.afa0bdfd.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunksaga3_ms=self.webpackChunksaga3_ms||[]).push([[189],{7125:function(s,t,e){e.r(t);var n=e(829),l=(e(2791),e(9434)),a=e(184);t.default=function(){console.log("AllPostsList");var s=(0,l.v9)((function(s){return s.allPosts.AllPosts}));return(0,a.jsxs)("div",{children:[(0,a.jsx)("h2",{className:"d-flex justify-content-center",children:"\u0421\u043f\u0438\u0441\u043e\u043a \u043f\u043e\u0441\u0442\u043e\u0432"}),(0,a.jsx)(n.Z,{PostsList:s})]})}}}]);
+//# sourceMappingURL=189.afa0bdfd.chunk.js.map
